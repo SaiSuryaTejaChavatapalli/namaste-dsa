e20d6249dc25b4651f7ff219a7806ac8d3f3d292
Sai Surya Teja <saisuryateja3@gmail.com>
2026-09-07 21:19:22 +0530

    Remove duplicates

---
+++ 6.remove-duplicates/remove-duplicates.js
@@ -0,0 +1,15 @@
+const removeDuplicates = function (nums) {
+  let x = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] > nums[x]) {
+      x = x + 1;
+      nums[x] = nums[i];
+    }
+  }
+
+  return x + 1;
+};
+
+console.log(removeDuplicates([1, 1, 2]));
+
+console.log(removeDuplicates([0, 0, 1, 1, 1, 2, 2, 3, 3, 4]));
